a29a43c2eea70d0b5218f98f6279798c69c63864
jusivo <justinaa9455@gmail.com>
2025-04-29 08:46:14 +0300

    update of order correct

--- src/main/java/com/kitchenplus/kitchenplus/data/services/OrderService.java
+++ src/main/java/com/kitchenplus/kitchenplus/data/services/OrderService.java
@@ -4,11 +4,13 @@ import com.kitchenplus.kitchenplus.data.enums.OrderStatus;
 import com.kitchenplus.kitchenplus.data.models.Item;
 import com.kitchenplus.kitchenplus.data.models.Order;
 import com.kitchenplus.kitchenplus.data.models.OrderLine;
+import com.kitchenplus.kitchenplus.data.models.User;
 import com.kitchenplus.kitchenplus.data.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class OrderService {
@@ -17,10 +19,9 @@ public class OrderService {
     public OrderService(OrderRepository orderRepository) {
         this.orderRepository = orderRepository;
     }
-    //fix:
-    public List<Order> getOrdersList(){
-
-        return orderRepository.findAll();
+    public List<Order> getOrdersList(User user) {
+        List<Order> orders_list = orderRepository.findOrdersByClient(user);
+        return orders_list;
     }
     public LocalDateTime getDateOfOrderPlacing(Long order_id){
         Order order = orderRepository.findById(order_id).orElse(null);
@@ -29,11 +30,12 @@ public class OrderService {
         }
         return null;
     }
-
     public  Order getSelectedOrderInformation(Long orderId){
         return orderRepository.findById(orderId).orElse(null);
     }
     public List<Order>getAllClientOrdersList(){
+        List<Order> ordersList = orderRepository.findAll();
+        System.out.println("Orders found: " + ordersList);
         return orderRepository.findAll();
     }
     public void insertOrder(Order order){
@@ -59,7 +61,19 @@ public class OrderService {
         orderRepository.save(order);
     }
     public Order updateOrder(Order order){
-        return orderRepository.save(order);
+        Order order_exist = orderRepository.findById(order.getId()).orElseThrow(()->new RuntimeException("Order not found"));
+        order_exist.setDateOfPlacing(order.getDateOfPlacing());
+        order_exist.setDateOfCompleted(order.getDateOfCompleted());
+        order_exist.setStatus(order.getStatus());
+        order_exist.setPointsApplied(order.getPointsApplied());
+        order_exist.setShippingCost(order.getShippingCost());
+        order_exist.setSumOfOrder(order.getSumOfOrder());
+        return orderRepository.save(order_exist);
+    }
+    public Order update(Order order){
+        Order order_exist = orderRepository.findById(order.getId()).orElseThrow(()->new RuntimeException("Order not found"));
+        order_exist.setStatus(OrderStatus.CANCELED);
+        return orderRepository.save(order_exist);
     }
     public Order getClientOrderDetails(Long client_id, Long order_id){
         Order order = orderRepository.findById(order_id).orElse(null);
