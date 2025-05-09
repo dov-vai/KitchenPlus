7e3d5e28425cb673952117188359afe456f159b5
jusivo <justinaa9455@gmail.com>
2025-05-09 22:07:22 +0300

    added repository method

--- src/main/java/com/kitchenplus/kitchenplus/data/repositories/DeliveryAddressRepository.java
+++ src/main/java/com/kitchenplus/kitchenplus/data/repositories/DeliveryAddressRepository.java
@@ -1,7 +1,12 @@
 package com.kitchenplus.kitchenplus.data.repositories;
 
+import com.kitchenplus.kitchenplus.data.models.Client;
 import com.kitchenplus.kitchenplus.data.models.DeliveryAddress;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
+    DeliveryAddress findDeliveryAddressByClient(Client client);
+
 }
--- src/main/java/com/kitchenplus/kitchenplus/data/repositories/OrderRepository.java
+++ src/main/java/com/kitchenplus/kitchenplus/data/repositories/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.kitchenplus.kitchenplus.data.repositories;
 
+import com.kitchenplus.kitchenplus.data.models.Client;
 import com.kitchenplus.kitchenplus.data.models.Order;
 import com.kitchenplus.kitchenplus.data.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findOrdersByClient(User user);
+    List<Order> findOrdersByClient(Client user);
 }
